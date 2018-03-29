57c3433a250423b21b18f0e6362c4e241c5dfe48
pingwinno <pingwinno@gmail.com>
2018-03-29 07:13:43 +0300

    Change protocol to MQTT

--- app/src/main/java/com/example/shiro/led_conrtol/MainActivity.java
+++ app/src/main/java/com/example/shiro/led_conrtol/MainActivity.java
@@ -15,6 +15,17 @@ import com.example.shiro.netCore.BroadcastSearch;
 import com.example.shiro.netCore.DataHelper;
 import com.example.shiro.netCore.JSONEntity;
 
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
@@ -24,9 +35,18 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
     private TextView mText;
     private DataHelper serverCon = null;
     private BroadcastSearch search = new BroadcastSearch();
-    private String ip = null;
+
     private static final String LOG_TAG = "Data Service";
     JSONEntity jsonEntity = new JSONEntity();
+
+    MqttAndroidClient mqttAndroidClient;
+
+    final String serverUri = "tcp://192.168.1.201:1883";
+
+    String clientId = "ExampleAndroidClient";
+    final String subscriptionTopic = "test";
+    final String publishTopic = "test";
+
 
 
 
@@ -86,34 +106,72 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         mText.setText("");
 
 
-        new Thread(new Runnable() {
+        clientId = clientId + System.currentTimeMillis();
+
+        mqttAndroidClient = new MqttAndroidClient(getApplicationContext(), serverUri, clientId);
+        mqttAndroidClient.setCallback(new MqttCallbackExtended() {
             @Override
-            public void run() {
-                try {
+            public void connectComplete(boolean reconnect, String serverURI) {
 
-                   ip = search.initFinder();
-
-                    Log.e(LOG_TAG, ip);
-                    if ( ip != null) {
-                        serverCon = new DataHelper(ip, 8000);
-                        Log.e(LOG_TAG, "connect");
-                        serverCon.openConnection();
-                        Thread.sleep(200);
-                        Log.e(LOG_TAG, "connected");
-                        Thread.sleep(200);
-                        Log.e(LOG_TAG, "send message");
-                    }
-                    else
-                    {
-                        Log.e(LOG_TAG, "Can't get ip address ");
-                        serverCon.closeConnection();
-                    }
-
-                } catch (Exception e) {
-                    Log.e(LOG_TAG, e.getMessage());
+                if (reconnect) {
+                    Log.e(LOG_TAG,"Reconnected to : " + serverURI);
+                    // Because Clean Session is true, we need to re-subscribe
+                    subscribeToTopic();
+                } else {
+                    Log.e(LOG_TAG,"Connected to: " + serverURI);
                 }
             }
-        }).start();
+
+            @Override
+            public void connectionLost(Throwable cause) {
+                Log.e(LOG_TAG,"The Connection was lost.");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.e(LOG_TAG,"Incoming message: " + new String(message.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setAutomaticReconnect(true);
+        mqttConnectOptions.setCleanSession(false);
+
+
+
+
+
+
+
+        try {
+            Log.e(LOG_TAG,"Connecting to " + serverUri);
+            mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
+                    disconnectedBufferOptions.setBufferEnabled(true);
+                    disconnectedBufferOptions.setBufferSize(100);
+                    disconnectedBufferOptions.setPersistBuffer(false);
+                    disconnectedBufferOptions.setDeleteOldestMessages(false);
+                    mqttAndroidClient.setBufferOpts(disconnectedBufferOptions);
+                    subscribeToTopic();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.e(LOG_TAG,"Failed to connect to: " + serverUri);
+                }
+            });
+
+
+        } catch (MqttException ex){
+            ex.printStackTrace();
+        }
 
 
     }
@@ -121,40 +179,7 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
     {
-        if (isChecked == true)
-        {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try
-                    {
-                        serverCon.sendData(Base64Converter.encode("ON"));
-                    }
-                    catch (Exception e)
-                    {
-                        Log.e(LOG_TAG, e.getMessage());
-                    }
 
-                }
-            }).start();
-        }
-        else
-            {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try
-                        {
-                            serverCon.sendData(Base64Converter.encode("OfF"));
-                        }
-                        catch (Exception e)
-                        {
-                            Log.e(LOG_TAG, e.getMessage());
-                        }
-
-                    }
-                }).start();
-            }
 
     }
 
@@ -163,19 +188,15 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         @Override
         public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
-            Log.e(LOG_TAG, "sending data");
-            if (serverCon == null) {
-                Log.e(LOG_TAG, "server not exist");
-            }
+
             new Thread(new Runnable() {
                 @Override
                 public void run() {
                     try {
-                        jsonEntity.setJSON(0, mRedSeekBar.getProgress(),mGreenSeekBar.getProgress(),mBlueSeekBar.getProgress());
+                        jsonEntity.setJSON(0, mRedSeekBar.getProgress(), mGreenSeekBar.getProgress(), mBlueSeekBar.getProgress());
 
 
-
-                        serverCon.sendData( converter.encodeRGB(jsonEntity));
+                        publishMessage(converter.encodeRGB(jsonEntity));
 
                     } catch (Exception e) {
                         Log.e(LOG_TAG, e.getMessage());
@@ -183,8 +204,8 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
 
                 }
             }).start();
-        }
 
+        }
 
         @Override
         public void onStartTrackingTouch(SeekBar seekBar) {
@@ -224,7 +245,56 @@ protected void onDestroy ()
 }
 
 
+    public void subscribeToTopic(){
+        try {
+            mqttAndroidClient.subscribe(subscriptionTopic, 0, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.e(LOG_TAG,"Subscribed!");
+                }
 
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.e(LOG_TAG,"Failed to subscribe");
+                }
+            });
+
+            // THIS DOES NOT WORK!
+            mqttAndroidClient.subscribe(subscriptionTopic, 0, new IMqttMessageListener() {
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    // message Arrived!
+                    System.out.println("Message: " + topic + " : " + new String(message.getPayload()));
+                }
+            });
+
+        } catch (MqttException ex){
+            System.err.println("Exception whilst subscribing");
+            ex.printStackTrace();
+        }
+    }
+
+    public void publishMessage(String jsonString){
+
+        if (mqttAndroidClient.isConnected()) {
+            try {
+                MqttMessage message = new MqttMessage();
+                message.setPayload(jsonString.getBytes());
+                mqttAndroidClient.publish(publishTopic, message);
+                Log.e(LOG_TAG, "Message Published");
+                if (!mqttAndroidClient.isConnected()) {
+                    Log.e(LOG_TAG, mqttAndroidClient.getBufferedMessageCount() + " messages in buffer.");
+                }
+            } catch (MqttException e) {
+                System.err.println("Error Publishing: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+        else
+        {
+            Log.e(LOG_TAG,"Failed to publishing. Connection not exist");
+        }
+    }
 
 
 
